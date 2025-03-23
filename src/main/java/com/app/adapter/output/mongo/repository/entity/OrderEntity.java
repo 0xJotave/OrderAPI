b7a794fd7e7e618d7ce215028c55ca0d1c845c9c
0xJotave <joaovmb15@gmail.com>
2025-03-23 00:33:10 -0300

    Adicionando atributo status na Entity de Order

--- src/main/java/com/app/adapter/output/mongo/repository/entity/OrderEntity.java
+++ src/main/java/com/app/adapter/output/mongo/repository/entity/OrderEntity.java
@@ -1,6 +1,7 @@
 package com.app.adapter.output.mongo.repository.entity;
 
 import com.app.domain.model.Item;
+import com.app.domain.model.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,15 +20,17 @@ public class OrderEntity {
     private String orderExternalId;
     private List<Item> Items;
     private float totalPrice;
+    private OrderStatus status;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private LocalDateTime createdAt;
 
     public OrderEntity(String orderId, String orderExternalId, List<Item> items, float totalPrice,
-                       LocalDateTime createdAt) {
+                       OrderStatus status, LocalDateTime createdAt) {
         this.orderId = orderId;
         this.orderExternalId = orderExternalId;
         Items = items;
         this.totalPrice = totalPrice;
+        this.status = status;
         this.createdAt = createdAt;
     }
 
@@ -64,6 +67,14 @@ public class OrderEntity {
 
     public void setTotalPrice(float totalPrice) {
         this.totalPrice = totalPrice;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
     }
 
     public LocalDateTime getCreatedAt() {
