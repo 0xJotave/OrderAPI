d5d0363a7b548d20babbf7849bd10b2682307d4d
0xJotave <joaovmb15@gmail.com>
2025-03-23 03:08:32 -0300

    Limpando Imports e Métodos

--- src/main/java/com/app/adapter/input/controller/OrderController.java
+++ src/main/java/com/app/adapter/input/controller/OrderController.java
@@ -4,20 +4,21 @@ import com.app.adapter.input.dto.OrderDTO;
 import com.app.adapter.input.mapper.OrderMapper;
 import com.app.domain.model.Order;
 import com.app.domain.service.OrderService;
+import com.app.port.input.OrderControllerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-public class OrderController {
+public class OrderController implements OrderControllerPort {
     private final OrderService orderService;
 
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
 
+    @Override
     @PostMapping("/orders")
     public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderDTO orderDTO) {
         Order order = OrderMapper.toModel(orderDTO);
@@ -25,24 +26,28 @@ public class OrderController {
         return ResponseEntity.ok(OrderMapper.toDTO(createdOrder));
     }
 
+    @Override
     @GetMapping("/orders")
     public ResponseEntity<List<OrderDTO>> getAllOrders() {
         List<OrderDTO> orders = orderService.getAllOrders().stream().map(OrderMapper::toDTO).toList();
         return ResponseEntity.ok(orders);
     }
 
+    @Override
     @GetMapping("/orders/{id}")
      public ResponseEntity<OrderDTO> getById(@PathVariable String id) {
         return orderService.getSingleOrder(id).map(order -> ResponseEntity.ok(OrderMapper.toDTO(order)))
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Override
     @DeleteMapping("/orders/{id}")
     public ResponseEntity<String> deleteById(@PathVariable String id) {
         orderService.deleteOrder(id);
         return ResponseEntity.ok("Order " + id + " Deleted");
     }
 
+    @Override
     @DeleteMapping("/orders")
     public ResponseEntity<String> deleteAllOrders() {
         orderService.deleteAllOrders();
