3db56efc49fbb0dd0129ee2aaac378df910e369d
0xJotave <joaovmb15@gmail.com>
2025-03-23 00:31:18 -0300

    Modificando Nome

--- src/main/java/com/app/OrderApiApplication.java
+++ src/main/java/com/app/OrderApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderapiApplication {
+public class OrderApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderapiApplication.class, args);
+		SpringApplication.run(OrderApiApplication.class, args);
 	}
 
 }
